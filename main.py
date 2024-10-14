8a0619f7c2264330870e545d73c4993d911b14a3
WillyShpilly <chebotarev@dmitry.by>
2024-10-14 13:38:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# name: str = 'Dmitry'
+# print('Hello, ' + name)
+# print(f'Hello, {name}')
+# print("hello, "'Dmirty')
+#
+# print(name * 5)
+# print('Hell no' * 5)
+#
+# name = int(input('Enter your name: '))
+# familia = 'Chebotarev'
+# print(name + familia)
+# name = 'Dmitry'
+# print(name[:4])
+
+example = 'HelloWorld'
+print(example[0])
+print(example[-1])
+print(example[2:])
+print(example[::-1])
+print(len(example))
+print(example[1:10:2])
